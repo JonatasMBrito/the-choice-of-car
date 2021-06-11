b25c48e3063de7a94d7e0c5cc1ffbed80c243e73
Jonatas Moraes Brito <jonatas.moraes@institutounibancovso.onmicrosoft.com>
2021-06-11 01:23:24 -0300

    base

---
+++ src/main/java/br/com/jonatasmbrito/thechoiceofcar/TheChoiceOfCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonatasmbrito.thechoiceofcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheChoiceOfCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheChoiceOfCarApplication.class, args);
+	}
+
+}
